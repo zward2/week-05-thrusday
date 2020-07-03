d84a9d6c6fddc298ec9589c596e0b221617b33e1
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2020-07-02 20:18:23 -0400

    Answers two of four excercises

--- all.js
+++ all.js
@@ -1,6 +1,11 @@
 function all(arr) {
 	// Loop through every item in the array
 	// If the item evaluates to false, return false
-
+for (i=0;i <arr.length; i++){
+	if (arr(i) === false) {
+		return false
+	}
+}
 	// If the loop ends, return true
+	return true;
 }
--- xor.js
+++ xor.js
@@ -1,7 +1,12 @@
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
-
+	if (cond1 === true && cond2 === false){
+		return true
+	}
 	// if cond1 is false and cond2 is true return true
-
-	// return false
+	if (cond1=== false && cond2 === true){
+		return true
+	}
+	
+	return false
 }
