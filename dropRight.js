182e640bc7baca2dc0c53961bcaec6ea9ad9c8f7
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2020-07-06 21:37:47 -0400

    Answers to last excercises

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,10 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
-
-	// If the loop ends, return true
-}
+function dropRight(arr, n) {	
+	// if n is larger than array length return []
+	if(n > arr.length ){
+		return arr;
+	}
+	// Use n to count in from the end to get the index
+	let index= (arr.length - n);
+	// return a slice of the array using the index as the end
+		return arr.slice(0, index);
+	}
--- flatten.js
+++ flatten.js
@@ -1,9 +1,15 @@
 function flatten(arr) {
 	// Create a new array
-
+	let z = [] 
 	// Loop through every item in the array
+	for (i=0; i<arr.length; i++) {
+		if (isArray(arr[i]))
+	}
+
 	// If the item is an array, add its items to the new array
+
 	// If not add the item itself to the new array
 
 	// return the new array
+		return z
 }
